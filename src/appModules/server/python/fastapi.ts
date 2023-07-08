841f7b5743f1162bc286407987af9a406f929b64
Mikhail Kraevskii <kramihm@gmail.com>
2023-07-08 23:51:38 +0700

    Fastapi component

---
+++ src/appModules/server/python/fastapi.ts
@@ -0,0 +1,23 @@
+export default class FastAPIComponent implements ServerButlerComponent {
+    name: string;
+    useDocker: boolean;
+    private connections: Set<ButlerComponent> = new Set<ButlerComponent>()
+
+
+    constructor(name: string, useDocker: boolean) {
+        this.name = name;
+        this.useDocker = useDocker;
+    }
+
+    cancelConnection(component: ButlerComponent): void {
+        this.connections.delete(component)
+    }
+
+    connect(component: ButlerComponent): void {
+        this.connections.add(component)
+    }
+
+    getConnections(): ButlerComponent[] {
+        return Array.from(this.connections)
+    }
+}
